9e2aa4bdead8cae2393d7f321ed118bc114707d1
Kaiyuan Liu <jungle.chuan@gmail.com>
2015-11-01 22:19:20 +0000

    add Config and Newsweaver

--- src/main/java/com/newsweaver/Config.java
+++ src/main/java/com/newsweaver/Config.java
@@ -1,4 +1,62 @@
 package com.newsweaver;
 
 public class Config {
+    private final String username;
+    private final String password;
+    private final String userAgent;
+    private final String accountCode;
+
+    public Config(Builder builder)
+    {
+        this.username = builder.username;
+        this.password = builder.password;
+        this.userAgent = builder.userAgent;
+        this.accountCode = builder.accountCode;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public String getAccountCode() {
+        return accountCode;
+    }
+
+    public static class Builder {
+        private String username;
+        private String password;
+        private String userAgent;
+        private String accountCode;
+
+        public Builder(String username, String password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public Builder userAgent(String userAgent)
+        {
+            this.userAgent = userAgent;
+            return this;
+        }
+
+        public Builder accountCode(String accountCode)
+        {
+            this.accountCode = accountCode;
+            return this;
+        }
+
+        public Config build()
+        {
+            return new Config(this);
+        }
+    }
 }
--- src/main/java/com/newsweaver/Newsweaver.java
+++ src/main/java/com/newsweaver/Newsweaver.java
@@ -1,4 +1,81 @@
 package com.newsweaver;
 
+import com.newsweaver.http.ClientInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Newsweaver {
+
+    public static final String VERSION = "1.0";
+
+    public static final String LIVE_API_BASE = "https://api.newsweaver.com";
+
+    protected Config config;
+
+    protected List<ClientInterface> clients = new ArrayList<ClientInterface>();
+
+    protected RequestOption option;
+
+    public Newsweaver(String username, String password, String accountCode) {
+        this(new Config.Builder(username, password)
+                .accountCode(accountCode)
+                .build());
+    }
+
+    public Newsweaver(Config config) {
+        this(config, new ArrayList<ClientInterface>());
+    }
+
+    public Newsweaver(Config config, List<ClientInterface> clients) {
+        this(config, clients, new RequestOption());
+    }
+
+    public Newsweaver(Config config, List<ClientInterface> clients, RequestOption option) {
+        this.config = config;
+        this.clients = clients;
+        this.option = option;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public Newsweaver setConfig(Config config) {
+        this.config = config;
+        return this;
+    }
+
+    public Newsweaver pushClient(ClientInterface client) {
+        this.clients.add(0, client);
+        return this;
+    }
+
+    public ClientInterface popClient() {
+        if (clients.isEmpty()) {
+            throw new IllegalArgumentException("You tried to pop from an empty client stack");
+        }
+        return clients.remove(0);
+    }
+
+    public Newsweaver setClients(List<ClientInterface> clients) {
+        this.clients = new ArrayList<ClientInterface>();
+        for (ClientInterface client: clients) {
+            this.pushClient(client);
+        }
+        return this;
+    }
+
+    public List<ClientInterface> getClients() {
+        return clients;
+    }
+
+    public Newsweaver setOption(RequestOption option) {
+        this.option = option;
+        return this;
+    }
+
+    public RequestOption getOption() {
+        return option;
+    }
 }
